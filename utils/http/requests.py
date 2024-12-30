8a99bb78c24920fe5dd2264387b7dd004e985cc6
charles <lucky.goldragon@gmail.com>
2024-12-30 03:26:59 -0500

    udpate http requests

--- utils/http/requests.py
+++ utils/http/requests.py
@@ -1,6 +1,7 @@
 import requests
 from config import BASE_ENDPOINT_URL
-from enums import INGRESS_VIDEO_STATUS
+from enums import INGRESS_VIDEO_STATUS, SEARCH_QUERY_STATUS
+from utils.logger import download_logger, scrapping_logger
 
 
 def http_update_ingress_video(data):
@@ -14,19 +15,33 @@ def http_update_ingress_video(data):
             print("Error in http_update_ingress", response.text)
             return None
     except Exception as e:
-        print("Error in http_update_ingress", e)
+        download_logger("Error in http_update_ingress: %s", e)
         return None
 
 
-def http_get_pending_ingress_videos():
-    url = f"{BASE_ENDPOINT_URL}/api/v1/ingress-videos/list-ingress-videos?status={INGRESS_VIDEO_STATUS.PENDING}"
+def http_get_pending_ingress_videos(offset: int = 0, limit: int = 1):
+    url = f"{BASE_ENDPOINT_URL}/api/v1/ingress-videos/list-ingress-videos?status={INGRESS_VIDEO_STATUS.PENDING}&offset={offset}&limit={limit}"
     headers = {"Content-Type": "application/json"}
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.json()["data"]
-        print("Error in http_get_pending_ingress_videos", response.text)
+        download_logger("Error in http_get_pending_ingress_videos: %s", response.text)
         return None
     except Exception as e:
-        print("Error in http_get_pending_ingress_videos", e)
+        download_logger("Error in http_get_pending_ingress_videos: %s", e)
+        return None
+
+
+def http_get_pending_query(offset: int = 0, limit: int = 1):
+    url = f"{BASE_ENDPOINT_URL}/api/v1/search-queries/list-search-queries?status={SEARCH_QUERY_STATUS.PENDING}&offset={offset}&limit={limit}"
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()["data"]
+        scrapping_logger("Error in http_get_pending_query: %s", response.text)
+        return None
+    except Exception as e:
+        scrapping_logger("Error in http_get_pending_query: %s", e)
         return None

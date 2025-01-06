8049a433ed54a6a0f5c86e6e7c03ac21d9c4b7af
charles <lucky.goldragon@gmail.com>
2025-01-06 05:48:58 +0000

    update process_pending_queries

--- utils/video_scrapping/video_scrapping_worker.py
+++ utils/video_scrapping/video_scrapping_worker.py
@@ -15,12 +15,20 @@ class VideoScrappingWorker:
         self.max_workers = 4
         VideoScrapper.proxies = PROXIES
 
-    async def process_pending_downloads(self):
+    async def process_pending_queries(self):
         pending_queries = http_get_pending_query()
         if pending_queries and len(pending_queries) > 0:
             for pending_query in pending_queries:
                 query = pending_query["query"]
                 extracted_video_urls = VideoScrapper.scrapping_video_urls_with_search_query(query)
+
+                # If the extracted_video_urls is None or empty array, it will update the status to failed
+                if not extracted_video_urls:
+                    pending_query["status"] = SEARCH_QUERY_STATUS.FAILED
+                    http_update_query(pending_query)
+
+                pending_query["status"] = SEARCH_QUERY_STATUS.EXTRACTING
+                http_update_query(pending_query)
 
                 for video_url in extracted_video_urls:
                     video_metadata = VideoScrapper.get_video_metadata(video_url)
@@ -30,24 +38,28 @@ class VideoScrappingWorker:
                         # and update the status to extracted
                         video_metadata["game_id"] = pending_query["game_id"]
                         http_add_ingress_video(video_metadata)
-                        pending_query["status"] = SEARCH_QUERY_STATUS.EXTRACTED
-                        http_update_query(pending_query)
+
                     else:
                         # If extracting video metadata is None, it will update the status to failed and break the loop
                         pending_query["status"] = SEARCH_QUERY_STATUS.FAILED
                         http_update_query(pending_query)
                         break
-                    
+
                     # sleep for 3 seconds to allow the scrippint to avoid blocking from youtube
                     await asyncio.sleep(3)
+                
+                if pending_query["status"] == SEARCH_QUERY_STATUS.EXTRACTING:
+                    pending_query["status"] = SEARCH_QUERY_STATUS.EXTRACTED
+                    http_update_query(pending_query)
         else:
             scrapping_logger.info(f"There is no pending query, will sleep for {self.fetch_interval} seconds...")
             await asyncio.sleep(self.fetch_interval)
+    
     async def run(self):
         self.is_running = True
         while self.is_running:
             try:
-                await self.process_pending_downloads()
+                await self.process_pending_queries()
             except Exception as e:
                 self.is_running = False
                 scrapping_logger.error("Error in VideoScrappingWorker: %s", e)
@@ -55,8 +67,8 @@ class VideoScrappingWorker:
 
     async def stop(self):
         self.is_running = False
-        # TODO: Add cleanup logic to stop all active downloads
-        scrapping_logger.info("Video download worker stopped")
+        # TODO: Add cleanup logic to stop all active queries
+        scrapping_logger.info("Video scrapping worker stopped")
 
 
 def run_worker():

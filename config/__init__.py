3a8ef688ac717fbb4594dd5f7624f7c93a1468cd
charles <lucky.goldragon@gmail.com>
2025-01-06 00:17:39 +0000

    add proxies and cookie path into env and config

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import json
 import os
 
 load_dotenv()
@@ -6,3 +7,8 @@ load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
 VIDEO_DOWNLOAD_DIR = os.getenv("VIDEO_DOWNLOAD_DIR")
 BASE_ENDPOINT_URL = os.getenv("BASE_ENDPOINT_URL")
+COOKIES_PATH = os.getenv("COOKIES_PATH")
+
+PROXIES = []
+with open("proxies.json", "r") as f:
+    PROXIES = json.load(f) 

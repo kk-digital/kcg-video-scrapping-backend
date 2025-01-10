88f63e24dd58d4c201e847f758e36a1586fa7bd5
charles <lucky.goldragon@gmail.com>
2025-01-10 01:34:33 -0500

    add http_get_downloadding_ingress_videos to requests

--- utils/http/requests.py
+++ utils/http/requests.py
@@ -44,6 +44,19 @@ def http_get_pending_ingress_videos(offset: int = 0, limit: int = 1):
         request_logger.error("Error in http_get_pending_ingress_videos: %s", e)
         return None
 
+def http_get_downloadding_ingress_videos(offset: int = 0, limit: int = 1):
+    url = f"{BASE_ENDPOINT_URL}/api/v1/ingress-videos/list-ingress-videos?status={INGRESS_VIDEO_STATUS.DOWNLOADINDG}&offset={offset}&limit={limit}"
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()["data"]
+        request_logger.error("Error in http_get_downloadding_ingress_videos: %s", response.text)
+        return None
+    except Exception as e:
+        request_logger.error("Error in http_get_downloadding_ingress_videos: %s", e)
+        return None
+
 def http_update_query(data):
     url = f"{BASE_ENDPOINT_URL}/api/v1/search-queries/update-search-query"
     headers = {"Content-Type": "application/json"}
@@ -78,10 +91,10 @@ def http_get_extracting_query(offset: int = 0, limit: int = 10):
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             return response.json()["data"]
-        request_logger.error("Error in http_get_pending_query: %s", response.text)
+        request_logger.error("Error in http_get_extracting_query: %s", response.text)
         return None
     except Exception as e:
-        request_logger.error("Error in http_get_pending_query: %s", e)
+        request_logger.error("Error in http_get_extracting_query: %s", e)
         return None
     
 def http_check_existence_videos(video_ids: list):

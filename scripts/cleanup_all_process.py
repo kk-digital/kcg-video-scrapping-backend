ee62c461294525f670ee195bc1418efab3c4a4f5
charles <lucky.goldragon@gmail.com>
2025-01-09 20:09:05 -0500

    add script for cleanup all python process

---
+++ scripts/cleanup_all_process.py
@@ -0,0 +1,18 @@
+import psutil
+
+def kill_python_workers():
+    for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+        try:
+            # Check if it's a Python process
+            if 'python' in proc.info['name'].lower():
+                print(f"Found Python process: {proc.info['pid']} {proc.info['name']} {proc.info['cmdline']}")
+                cmdline = proc.info['cmdline']
+                if cmdline and any('kcg' in arg.lower() for arg in cmdline):
+                    proc.terminate()
+                    print(f"Terminated process {proc.info['pid']}")
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            print(f"Failed to terminate process {proc.info['pid']}")
+            pass
+
+if __name__ == "__main__":
+    kill_python_workers()
